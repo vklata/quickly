c3c59795c80964872346b34e5146db3b9be9e868
vklata <151921976+vklata@users.noreply.github.com>
2024-07-14 02:31:43 +0530

    Frist Commit

--- backend2/index.js
+++ backend2/index.js
@@ -7,13 +7,17 @@ import productRoutes from './Routes/productRoutes.js'
 import paymentRoutes from './Routes/paymentRoutes.js'
 connectDB();
 import cors from 'cors'
+import mongoose, { mongo } from 'mongoose'
 const app = express();
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:5173'
+  // origin: 'http://localhost:5173'
+  origin:["https://deploy-mern-1whq.vercel.app"],
+  methods:["POST","GET"],
+  credentials:true
 }));
-
+mongoose.connect('mongodb+srv://vishnukaushik173:kklata123@cluster0.wzxhdvu.mongodb.net/orderquick?retryWrites=true&w=majority&appName=Cluster0')
 app.use('/api',authRoutes)
 app.use('/api',categoryRoutes)
 app.use('/api',productRoutes)
